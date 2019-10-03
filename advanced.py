6fe797b23ad3356dd9e4c3c169c1504dc1f7b8e1
James Liu <jamesliu1991@gmail.com>
2019-10-03 10:50:47 -0700

    test commit

--- advanced.py
+++ advanced.py
@@ -1,24 +1,29 @@
-# Advanced function(s)
-## Code Attribution: Code snippets taken and adapted from https://towardsdatascience.com/how-to-be-pythonic-and-why-you-should-care-188d63a5037e
+# Advanced function(s) # Code Attribution: Code snippets taken and adapted from
+# https://towardsdatascience.com/how-to-be-pythonic-and-why-you-should-care-188d63a5037e
 
-def even_value_doubler(array_to_double = [1,2,3,5,6,7,9,10,12]):
+def even_value_doubler(array_to_double=None):
     """ 
     This function allegedly takes an list array of numbers and doubles the value of every even value in the list.
     """
 
-	length = len(array_to_double)
-	for i in range(0, length):
-	    if arr[i] % 2 == 0:
-	        arr[i] *= 2
+    if array_to_double is None:
+        array_to_double = [1, 2, 3, 5, 6, 7, 9, 10, 12]
+    length = len(array_to_double)
+    for i in range(0, length):
+        if arr[i] % 2 == 0:
+            arr[i] *= 2
 
-	return arr
+    return arr
 
-def here_is_a_challenge(bar=[]): # bar is optional and defaults to [] if not specified
+
+def here_is_a_challenge(bar=None):  # bar is optional and defaults to [] if not specified
     """
     What is wrong with this function!? 
     Hint: Try running this a few times
 
     Attribution: Code taken directly from: https://www.toptal.com/python/top-10-mistakes-that-python-programmers-make
-    """ 
-	bar.append("baz")
-	return bar
+    """
+    if bar is None:
+        bar = []
+    bar.append("baz")
+    return bar
